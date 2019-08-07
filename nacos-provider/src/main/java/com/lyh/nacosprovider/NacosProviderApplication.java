35bdff3f9076b2b4033461fcbe9acc9906f9b5a1
hua-qiqi <540932787@qq.com>
2019-08-07 15:30:00 +0800

    nacos构建配置中心

--- nacos-provider/src/main/java/com/lyh/nacosprovider/NacosProviderApplication.java
+++ nacos-provider/src/main/java/com/lyh/nacosprovider/NacosProviderApplication.java
@@ -1,26 +1,37 @@
 package com.lyh.nacosprovider;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@RefreshScope
+@RestController
 public class NacosProviderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NacosProviderApplication.class, args);
     }
 
+    @Value("${age}")
+    private String age;
 
-    @RestController
-    static class TestController {
+
+
         @GetMapping("/hello")
         public String hello(@RequestParam String name) {
             return "hello " + name;
         }
+
+    @GetMapping("/age")
+    public String age() {
+        return "hello I am"+age+"years old";
     }
+
 }
